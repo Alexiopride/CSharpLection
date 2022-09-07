29f7319b7a292cf6cacb02ff0c5bdf801d6094a9
alexiopride <alexiopride@gmail.com>
2022-09-07 22:08:04 +0400

    Добавил задачи к 10 примеру

--- Example_010/Program.cs
+++ Example_010/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Имеется одномерный массив array из n элементов,
+// требуется найти элемент массива равный find.
+// 1. установить счётчик Index в позиции 0.
+// 2. Если array [index] = Find, алгоритм
+// завершил работу успешно.
+// 3. Увеличить index на 1.
+// 4. Если index < n,  то перейти к шагу 2. 
+// В противном случаеалгоритм завершил работу безуспешно.
+
+int[] array = {1, 12, 31, 4, 18, 15, 16, 17, 18};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1
+    index++;
+}
